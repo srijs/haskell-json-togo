d3fca92cc01c13dcc1440e110996cae80f0d1833
Sam Rijs <srijs@airpost.net>
2015-06-14 01:28:54 +1000

    use attoparsec-trans

--- src/Data/JSON/ToGo.hs
+++ src/Data/JSON/ToGo.hs
@@ -1,45 +1,67 @@
 module Data.JSON.ToGo
-  ( Value'(..), apply, apply_
+  ( ValueM(..)
+  , applyV, applyV_
+  , applyP, applyP_
   ) where
 
+import Data.JSON.ToGo.Parser
+
 import Data.Aeson (Value(..))
+import Data.Monoid (Monoid)
 import Data.Scientific (Scientific)
 import Data.Text (Text)
 import qualified Data.Vector as V
 import qualified Data.HashMap.Strict as H
 
 import Control.Monad (MonadPlus, mzero, msum)
+import Control.Monad.Trans.Class (lift)
 
-data Value' m a
-  = Null'   (m a)
-  | Bool'   (Bool -> m a)
-  | Number' (Scientific -> m a)
-  | String' (Text -> m a)
-  | Array'  (Int -> Value' m a)
-  | Object' (Text -> Value' m a)
+data ValueM m a
+  = NullM   (m a)
+  | BoolM   (Bool -> m a)
+  | NumberM (Scientific -> m a)
+  | StringM (Text -> m a)
+  | ArrayM  (Int -> ValueM m a)
+  | ObjectM (Text -> ValueM m a)
+  | AnyM    (Value -> m a)
 
-instance Functor m => Functor (Value' m) where
-  fmap g (Null' ma)  = Null'   $ fmap g ma
-  fmap g (Bool' f)   = Bool'   $ fmap (fmap g) f
-  fmap g (Number' f) = Number' $ fmap (fmap g) f
-  fmap g (String' f) = String' $ fmap (fmap g) f
-  fmap g (Array' f)  = Array'  $ fmap (fmap g) f
-  fmap g (Object' f) = Object' $ fmap (fmap g) f
+instance Monad m => Functor (ValueM m) where
+  fmap g (NullM ma)  = NullM   $ ma >>= return.g
+  fmap g (BoolM f)   = BoolM   $ fmap (>>= return.g) f
+  fmap g (NumberM f) = NumberM $ fmap (>>= return.g) f
+  fmap g (StringM f) = StringM $ fmap (>>= return.g) f
+  fmap g (ArrayM f)  = ArrayM  $ fmap (fmap g) f
+  fmap g (ObjectM f) = ObjectM $ fmap (fmap g) f
+  fmap g (AnyM f)    = AnyM    $ fmap (>>= return.g) f
 
-apply :: MonadPlus m => Value' m a -> Value -> m a
-apply (Null' ma) Null = ma
-apply (Bool' f) (Bool b) = f b
-apply (Number' f) (Number n) = f n
-apply (String' f) (String s) = f s
-apply (Array' f) (Array v) = msum $ map (uncurry (apply . f)) (V.toList $ V.indexed v)
-apply (Object' f) (Object h) = msum $ map (uncurry (apply . f)) (H.toList h)
-apply _ _ = mzero
+applyV :: MonadPlus m => ValueM m a -> Value -> m a
+applyV (NullM ma)  Null       = ma
+applyV (BoolM f)   (Bool b)   = f b
+applyV (NumberM f) (Number n) = f n
+applyV (StringM f) (String s) = f s
+applyV (ArrayM f)  (Array v)  = msum $ map (uncurry (applyV . f)) (V.toList $ V.indexed v)
+applyV (ObjectM f) (Object h) = msum $ map (uncurry (applyV . f)) (H.toList h)
+applyV (AnyM f)    v          = f v
+applyV _           _          = mzero
 
-apply_ :: Monad m => Value' m a -> Value -> m ()
-apply_ (Null' ma) Null = ma >> return ()
-apply_ (Bool' f) (Bool b) = f b >> return ()
-apply_ (Number' f) (Number n) = f n >> return ()
-apply_ (String' f) (String s) = f s >> return ()
-apply_ (Array' f) (Array v) = mapM_ (uncurry (apply_ . f)) (V.toList $ V.indexed v)
-apply_ (Object' f) (Object ias) = mapM_ (uncurry (apply_ . f)) (H.toList ias)
-apply_ _ _ = return ()
+applyV_ :: Monad m => ValueM m a -> Value -> m ()
+applyV_ (NullM ma)  Null         = ma >> return ()
+applyV_ (BoolM f)   (Bool b)     = f b >> return ()
+applyV_ (NumberM f) (Number n)   = f n >> return ()
+applyV_ (StringM f) (String s)   = f s >> return ()
+applyV_ (ArrayM f)  (Array v)    = mapM_ (uncurry (applyV_ . f)) (V.toList $ V.indexed v)
+applyV_ (ObjectM f) (Object ias) = mapM_ (uncurry (applyV_ . f)) (H.toList ias)
+applyV_ (AnyM f)    v            = f v >> return ()
+applyV_ _           _            = return ()
+
+applyP :: (Monad m, Monoid r) => ValueM m r -> ParserM m r
+applyP (ArrayM f)  = parray  (applyP.f)
+applyP (ObjectM f) = pobject (applyP.f)
+applyP (NullM m)   = pbool   >>  lift m
+applyP (BoolM f)   = pbool   >>= lift.f
+applyP (NumberM f) = pnumber >>= lift.f
+applyP (StringM f) = pstring >>= lift.f
+applyP (AnyM f)    = pvalue  >>= lift.f
+
+applyP_ :: Monad m => ValueM m a -> ParserM m ()
+applyP_ = applyP . fmap (const ())
--- src/Data/JSON/ToGo/Parser.hs
+++ src/Data/JSON/ToGo/Parser.hs
@@ -1,42 +1,65 @@
 {-# LANGUAGE OverloadedStrings, RankNTypes #-}
 
-module Data.JSON.ToGo.Parser (parse) where
+module Data.JSON.ToGo.Parser where
 
 import Prelude hiding (sequence)
 
-import Data.JSON.ToGo (Value'(..))
-
-import Data.Aeson.Parser (jstring)
-import Data.Attoparsec.ByteString.Char8 (skipSpace, char, string, scientific)
+import Data.Aeson.Parser (jstring, value)
+import Data.Aeson.Types (Value)
+import Data.Attoparsec.ByteString.Char8 (skipSpace, char, string, scientific, parse)
 import Data.ByteString (ByteString)
-import Data.Conduit (Conduit, yield)
-import Data.Conduit.Attoparsec (sinkParser)
+import Data.Monoid (Monoid, mempty, (<>))
+import Data.Scientific (Scientific)
+import Data.Text (Text)
 
-import Control.Applicative (pure, (<$), (<$>), (<*), (*>), (<|>))
-import Control.Monad (mzero, join)
-import Control.Monad.Catch (MonadThrow)
-import Control.Monad.Trans.Class (lift)
+import Control.Applicative ((<$), (<*), (<|>))
+import Control.Monad (join)
+import Control.Monad.Trans.Parser (ParserT, liftP)
 
-parse :: MonadThrow m => Value' m a -> Conduit ByteString m a
-parse (Array' f) = sinkParser (skipSpace >> char '[') >> go 0
+type ParserM = ParserT ByteString
+
+rP p = liftP . parse $ skipSpace >> p
+
+parray :: (Monad m, Monoid r) => (Int -> ParserM m r) -> ParserM m r
+parray f = rP (char '[') >> go 0 mempty
   where
-    go idx = do
-      parse (f idx)
-      join $ sinkParser $ skipSpace >>
-        (go (succ idx) <$ char ',') <|> (return () <$ char ']')
-parse (Object' f) = sinkParser (skipSpace >> char '{') >> go
+    go idx r = do
+      r' <- f idx
+      join $ rP $
+        (go (succ idx) (r <> r') <$ char ',') <|> (return (r <> r') <$ char ']')
+
+pobject :: (Monad m, Monoid r) => (Text -> ParserM m r) -> ParserM m r
+pobject f = rP (char '{') >> go mempty
   where
-    go = do
-      key <- sinkParser $ skipSpace >>
-        jstring <* (skipSpace >> char ':')
-      parse (f key)
-      join $ sinkParser $ skipSpace >>
-        (go <$ char ',') <|> (return () <$ char '}')
-parse v = sinkParser (skipSpace >> prim v) >>= lift >>= yield
-  where
-    prim (Null' ma)  = ma <$ string "null"
-    prim (Bool' f)   = f <$> (string "false" *> pure False <|>
-                              string "true"  *> pure True)
-    prim (Number' f) = f <$> scientific
-    prim (String' f) = f <$> jstring
-    prim _           = mzero
+    go r = do
+      key <- rP $ jstring <* (skipSpace >> char ':')
+      r' <- f key
+      join $ rP $
+        (go (r <> r') <$ char ',') <|> (return (r <> r') <$ char '}')
+
+parrayL :: Monad m => (Int -> ParserM m r) -> ParserM m [r]
+parrayL = parray . fmap (fmap (:[]))
+
+pobjectL :: Monad m => (Text -> ParserM m r) -> ParserM m [r]
+pobjectL = pobject . fmap (fmap (:[]))
+
+parray_ :: Monad m => (Int -> ParserM m r) -> ParserM m ()
+parray_ = parray . fmap (fmap (const ()))
+
+pobject_ :: Monad m => (Text -> ParserM m r) -> ParserM m ()
+pobject_ = pobject . fmap (fmap (const ()))
+
+pnull :: Monad m => ParserM m ()
+pnull = rP $ () <$ string "null"
+
+pbool :: Monad m => ParserM m Bool
+pbool = rP $ False <$ string "false" <|> True <$ string "true"
+
+pnumber :: Monad m => ParserM m Scientific
+pnumber = rP scientific
+
+pstring :: Monad m => ParserM m Text
+pstring = rP jstring
+
+pvalue :: Monad m => ParserM m Value
+pvalue = rP value
